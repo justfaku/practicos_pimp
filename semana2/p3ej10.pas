217ee9f2cacffb6a5053c928137ea259c9e149dd
facu <fagus.mds@gmail.com>
2026-09-06 20:58:53 -0300

    mejora de la logica del codigo en p3ej10

--- semana2/p3ej10.pas
+++ semana2/p3ej10.pas
@@ -1,24 +1,38 @@
 program costopaquete;
 const
-    costo = 77;
-    costoad = 56;
+    costoBase = 77;
+    costoAd = 56;
 var
-    peso, fraccion: real; 
-    kgad: integer;
+    peso: real; 
+    kgAd, costoTotal: integer;
 begin
     write('Ingrese el peso del paquete: ');
     readln(peso);
 
-    kgad := trunc(peso) - 1;
-    fraccion := peso - trunc(peso);
-
-    if (peso < 0.0) then
+    //en caso de ingresar un peso invalido
+    if (peso <= 0.0) then
         writeln('El peso del paquete debe ser mayor a 0.0kg')
-    else if (peso <= 1.0) then
-        writeln(costo)
-        else if (fraccion <> 0) then
-        writeln(costo + costoad * kgad + costoad)
-            else
-                writeln(costo + costoad * kgad)
+
+    else 
+    begin
+        //el costo total siempre sera al menos el costo base
+        costoTotal := costoBase;
+
+        if (peso > 1.0) then
+        begin
+            //calculo los kg adicionales 
+            kgAd := trunc(peso - 1);
+
+            //verificar si existe parte fraccionaria
+            if (kgAd <> (peso - 1)) then
+                kgAd := kgAd + 1;
+
+            costoTotal := costoTotal + costoAd * kgAd
+        end;
+        
+    writeln('Peso', ' ', 'Costo');
+    writeln(peso:0:2, ' $', costoTotal)
+    end;
+
 end.
     
